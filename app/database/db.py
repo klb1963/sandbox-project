fbb50820e205b9ad6b20e68fb6de6c7bd66313b8
Leonid Kleimann <k_l_b_1963@hotmail.com>
2025-04-29 17:07:53 +0200

    Добавил миграцию базы в deploy.yml

--- app/database/db.py
+++ app/database/db.py
@@ -1,11 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")  # На старте можно использовать SQLite
+# Загружаем переменные из .env
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+# Собираем строку подключения к PostgreSQL
+DB_URL = (
+    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+)
+
+engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
