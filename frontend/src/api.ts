9d847fd5277ed4a7c407635b1bfd9c62774e3e1e
Leonid Kleimann <k_l_b_1963@hotmail.com>
2025-05-17 15:37:33 +0200

    ♻️ Refactor: Использование VITE_API_URL из .env

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://127.0.0.1:8000' // если сервер работает локально
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getCompanies = async () => {
   const response = await axios.get(`${API_URL}/companies`)
